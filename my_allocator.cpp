f9467fb54488fad5ec2c4af2c39a516f30398ccd
Jason Narvaez <thejason@tamu.edu>
2015-10-08 23:27:28 -0500

    Update my_allocator.cpp

--- my_allocator.cpp
+++ my_allocator.cpp
@@ -29,6 +29,7 @@
 #include <iostream>
 #include <bitset>
 #include <limits.h>
+#include <cstdint>
 
 /*--------------------------------------------------------------------------*/
 /* DATA STRUCTURES */ 
@@ -41,6 +42,7 @@
 /*--------------------------------------------------------------------------*/
 #pragma pack (1) // change the packing to 1 byte so no extra padding is added even on a 1 byte field
 struct header{
+	header(): next(NULL), size(8), free(true) {}
 	//header* buddy;//8 bytes
 	header* next;//8 bytes
 	int size;//4 bytes
@@ -53,7 +55,7 @@ header* memory;	//Entire memory allocated
 int memspace;	//amount of space available
 int basic;		//basic block size global variable
 const int header_size = sizeof(header);//UNCHANGEABLE SIZE OF HEADER CONST
-vector<header>free_list;
+vector<header*>free_list;
 /* free list indexing:
 [0] bbs
 [1] bbs * 2^1
@@ -76,31 +78,31 @@ so if total length is 128 and our bbs is 8, then our last index is n = 4
 
 /* Don't forget to implement "init_allocator" and "release_allocator"! */
 
-unsigned int bitFlip(unsigned int input)// input is the address, need to find the size of the address block
+unsigned int bitFlip(header* input)// input is the address, need to find the size of the address block
 {
 	//check for power
 	unsigned int check = 1;
 	int pwr;
-	/* 
-	while (check != input)
-	{
-		check *= 2;
-		pwr++;
-	}
-	pwr =-2;	//s-1th bit.  I subtract 2 here for the bit switch later.
-	 */
+	int block_addr;
+	int offset=input-memory;
+	header* output;
+	
 	//input would not work, you need to take the log2(size of memory block that input points to)
-	pwr = log2(input);//figure out the power of the input
+	//header* temp = input;
+	//header* inputptr = (header*) input;
+	cout<<input<<endl;
+	cout<<"input size: "<<input->size<<endl;
+	pwr = log2(input->size);//figure out the power of the input
 	pwr -=2;//s-1th bit. This bit will be flipped
     
-	//unsigned int output = input;
 	
-	bitset<8>output(input);//bitset<8> might not be enough bits
+	
+	output=input;
 	//cout<<output<<endl;
 	output ^= 1 << pwr;	//flips the pwrth bit (http://www.cplusplus.com/forum/beginner/34307/)
 	//cout<<output<<endl;
-	
-    return (unsigned int) (output.to_ulong());
+	cout<<output<<endl;
+    return (unsigned int) (output);
 }
 
 unsigned int init_allocator(unsigned int _basic_block_size, unsigned int _length)//i think this is wrong
@@ -125,21 +127,30 @@ memory made available to the allocator. If an error occurred, it returns 0. */
 		return 0;
 	}
 	else	{
-		header mem;
-		mem.size = block;
-		mem.next = NULL;
-		
+		//header mem;
+		/*header* memptr;
+	
+		memptr->size = block;
+		memptr->next = NULL;
+		*/
 		//use first() and last() to access free_list elements
 		cout<<"block size: "<<block<<endl;
 		cout<<"freesize: "<<freesize<<endl;
 		
 		//free_list.push_back(mem);
+		
 		memory = (header*)malloc(block);
 		//free_list.resize(freesize + 1);
-		
+		memory->size=block;
+		memory->free=true;
+		memory->next=NULL;
+
 		free_list.resize(freesize);
-		free_list[freesize-1] = mem;//put the allocated block into the appropriate free list
-		
+		free_list[freesize-1] = memory;//put the allocated block into the appropriate free list
+		//cout<<"mem size: "<<memptr->size<<endl;
+		cout<<"memory size: "<<free_list[free_list.size()-1]->size<<endl;
+		cout<<"memory address: "<<free_list[free_list.size()-1]<<endl;
+		cout<<"memory address next: "<<free_list[free_list.size()-1]->next<<endl;
 		basic = _basic_block_size;	//setting global basic block size
 		memspace = block;	//setting global total memory
 		return block;
@@ -168,12 +179,20 @@ extern Addr my_malloc(unsigned int _length) {// _length is memory requested by t
 	temp_val = temp_val / basic;// 32 / 8 = 4
 	temp_val = log2(temp_val);//contains the appropriate index in freelist
 	bool is_filled = true;
-	/* 
+	 
 	while(is_filled){
 	
-		if(free_list[temp_val].next == NULL){//there are no available blocks, start SPLITIN'
-			//for()
+		if(free_list[temp_val] == NULL){//there are no available blocks, start SPLITING
+			//uintptr_t myint = reinterpret_cast<uintptr_t>(&free_list[free_list.size()-1]);
+			//bitFlip(myint);
+			//bitFlip(free_list[free_list.size()-1]);
+			/* 
+			for(int i=0;i<free_list.size();i++){
+				if()
+			}
+			 */
 			
+			is_filled = false;//temporary, remove when finished
 		}
 		else{//WE FOUND IT!!!111!
 			
@@ -182,7 +201,7 @@ extern Addr my_malloc(unsigned int _length) {// _length is memory requested by t
 			
 		}
 	}
-	 */
+	 
 	cout<<"_length: "<<_length<<endl;
 	cout<<"tempsize: "<<tempsize<<endl;
 	//initialize block
@@ -201,6 +220,33 @@ extern int my_free(Addr _a) {
   return 0;
 }
 
+void PrintList(){
+	
+	for(int i=0;i<free_list.size();i++){
+		int count = 0;
+
+		header* current = NULL;
+		cout<<"size: "<<basic * pow(2,i)<<" ["<<i<<"]: ";
+		if(free_list[i] != NULL){
+			//cout<<"size: "<<basic * pow(2,i)<<" ["<<i<<"]: "<<free_list[i]->size<<endl;
+			
+			current = free_list[i];
+			while(current!=NULL){
+				count++;
+				cout<<count<<endl;
+				
+				current = current->next;
+			}
+			//cout<<free_list[i]->size<<endl;
+			
+		}
+		else{
+			cout<<"Empty!"<<endl;
+		}
+	}
+	
+}
+
 int main()
 {
 	/* int a = 126;
@@ -210,19 +256,24 @@ int main()
 	x ^=1 << 0;	//flips 1st bit
 	cout << x << endl; */
 	
-	
+	/* 
 	cout<<"=============================="<<endl
 		<<"bitFlip: "<<128<<"\n"<<endl;
 	cout<<bitFlip(128);
 	cout<<"header size:"<<endl;
-	header mainheader;
-	cout<<"size of header pointer: "<<sizeof(mainheader.next)<<endl
-		<<"size of int: "<<sizeof(mainheader.size)<<endl
-		<<"size of bool: "<<sizeof(mainheader.free)<<endl;
-	cout<<endl<<"size of header: "<<sizeof(mainheader)<<endl;
+	 */
+	
+	
+	
 	
 	init_allocator(8,64);
-	my_malloc(12);
+
+	cout<<"Print List:"<<endl;
+	PrintList();
+	bitFlip(memory);
+	//my_malloc(12);
+	
+	
 	
 	return 0;
 }
